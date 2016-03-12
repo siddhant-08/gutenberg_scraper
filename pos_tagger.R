eba714e0737fa5f178d39698493acd6be5d52479
Siddhant Singh <siddhantsingh707@gmail.com>
2016-03-12 19:17:45 +0530

    added code for installing koRpus

--- pos_tagger.R
+++ pos_tagger.R
@@ -1,5 +1,9 @@
-pos_tagger<-function(doc="korpusIP.txt"){
-  # doc is your text file
+pos_tagger<-function(doc){
+  # doc is the input text file
+  
+  if("koRpus" %in% rownames(installed.packages())==FALSE){
+    install.packages("koRpus")
+  }
   
   library(koRpus)
   
@@ -13,7 +17,7 @@ pos_tagger<-function(doc="korpusIP.txt"){
   # installation instructions at http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/
   tree_tagger_path<-"treetagger"
   
-  #returns an S4 object of type kRp.tsgged can be used further by extracting required slots
+  #returns an S4 object of type kRp.tagged and can be used further by extracting required slots
   # as given at http://finzi.psych.upenn.edu/library/koRpus/html/kRp.tagged-class.html
   
   tagged_text<-treetag(doc,treetagger="manual",lang=language,TT.options=list(path=tree_tagger_path,preset="en"))
@@ -23,6 +27,7 @@ pos_tagger<-function(doc="korpusIP.txt"){
   # The newer version of the treetagger instead ships with the file 'english-utf8.par'
   # So manually rename the file to 'english.par' in the treetag installation folder (tree_tagger_path/lib  in my case)
   ######################################################################################################################
+  
   #Results of the called tokenizer and POS tagger. The data.frame has eight columns:
   tags_df<-slot(tagged_text,"TT.res")
   
